d09f1106292ae0e6c3baaf8cb15ea1669c549f8e
Jacob Van Schenck <jacob.vanschenck@gmail.com>
2023-06-12 06:12:51 -0500

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-
+import netlify from '@astrojs/netlify/functions';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), react()],
+  integrations: [sitemap(), tailwind(), react()],
+  adapter: netlify(),
   output: 'server',
 });
