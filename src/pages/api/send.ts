43dfd377f32e630a76f3903a5a44663bf3b88d03
Jacob Van Schenck <jacob.vanschenck@gmail.com>
2023-06-12 21:34:48 -0500

    slack connection works

--- src/pages/api/send.ts
+++ src/pages/api/send.ts
@@ -1,4 +1,5 @@
 import type { APIRoute } from 'astro';
+import { WebClient } from '@slack/web-api';
 
 export const post: APIRoute = async ({ request }) => {
   const data = await request.formData();
@@ -12,5 +13,19 @@ export const post: APIRoute = async ({ request }) => {
 
   //lets use slack instead
 
-  return new Response(JSON.stringify({ message: 'success' }), { status: 200 });
+  try {
+    const web = new WebClient(import.meta.env.BOT_USER_TOKEN);
+    const res = await web.chat.postMessage({
+      channel: 'portfolio-contact-form',
+      text: `====================\n📥 *NEW MESSAGE*\n*email*: ${email}\n*message*: \n>${message}\n====================`,
+    });
+    console.log(res);
+    return new Response(JSON.stringify({ message: 'success' }), {
+      status: 200,
+    });
+  } catch (err) {
+    return new Response(JSON.stringify({ error: err }), {
+      status: 500,
+    });
+  }
 };
