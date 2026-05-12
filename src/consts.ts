d1303ef0e3a44796e89e6258a9e66e783c30a7cc
Jacob Van Schenck <jacob.vanschenck@gmail.com>
2026-05-11 21:11:41 -0500

    Copy and link updates

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'jacobvs.xyz';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION =
+    'Jacob Van Schenck is a full-stack software engineer building scalable product platforms, public APIs, backend architecture, and polished user experiences.';
